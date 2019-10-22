b3d4030c75538356acb4d8d2958b8aa278f1442c
huhubun <nongzc@outlook.com>
2019-10-22 23:08:52 +0800

    首次提交。 由于SEO需要，现在单独将博客前台部分通过 Nuxt 重构，使其能在服务器端渲染。

---
+++ layouts/default.vue
@@ -0,0 +1,99 @@
+<template>
+  <a-layout>
+    <a-affix :offsetTop="0">
+      <a-layout-header>
+        <a-menu mode="horizontal" theme="light">
+          <a-menu-item class="title bun-left">
+            <router-link to="/">呼呼小笼包的博客</router-link>
+          </a-menu-item>
+          <a-menu-item>
+            <router-link to="/about">关于</router-link>
+          </a-menu-item>
+          <a-menu-item>
+            <a-tooltip placement="bottom" title="(lll￢ω￢) 该功能尚未实装">
+              <a-input-search placeholder="搜索" />
+            </a-tooltip>
+          </a-menu-item>
+        </a-menu>
+      </a-layout-header>
+    </a-affix>
+    <a-layout-content>
+      <nuxt />
+    </a-layout-content>
+    <a-layout-footer>
+      <!-- <powered-by /> -->
+      <copyright />
+    </a-layout-footer>
+  </a-layout>
+</template>
+
+<script>
+import Copyright from "~/components/layout/Copyright.vue";
+
+export default {
+  components: {
+    Copyright
+  }
+};
+</script>
+
+<style lang="stylus">
+
+.bun-margin-top
+  margin-top: 8px
+
+.bun-margin-top-2x
+  margin-top: 16px
+
+.bun-margin-right
+  margin-right: 8px
+
+.bun-margin-right-2x
+  margin-right: 16px
+
+.bun-margin-bottom
+  margin-bottom: 8px
+
+.bun-margin-bottom-2x
+  margin-bottom: 16px
+
+.bun-margin-left
+  margin-left: 8px
+
+.bun-margin-left-2x
+  margin-left: 16px
+
+.bun-left
+  float: left
+  
+.ant-layout {
+  min-height: 100vh;
+}
+
+.ant-layout-header {
+  text-align: right;
+  background-color: #FFF;
+  box-shadow: 0 0 10px -5px #000;
+}
+
+.ant-input-search
+  text-align left
+
+.ant-layout-header .ant-menu.ant-menu-root {
+  border-bottom: 0;
+  padding-top: 8px;
+}
+
+.ant-layout-content {
+  background-color: #FFF;
+}
+
+.ant-layout-footer a {
+  color: rgba(0, 0, 0, 0.65);
+}
+
+.title {
+  font-size: 1.6em;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="post-list-container">
+    <post-list v-bind:list="list"></post-list>
+  </div>
+</template>
+
+<script>
+import PostList from "~/components/PostList.vue";
+
+export default {
+  data() {
+    return {
+    };
+  },
+  async asyncData({ $axios }) {
+    const list = await $axios.$get("/api/posts");
+
+    return { list };
+  },
+  components: {
+    PostList
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.post-list-container
+  padding: 0 100px
+</style>
